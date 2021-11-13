ccc38a352f6181327bafca5cbd32ef5299bc21b9
victoria-parker <victoriaparkerb@gmail.com>
2021-11-13 15:40:13 +0000

    formLogin added

---
+++ agencia/classes/Usuario.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Usuario
+{
+###Autenticacion##
+    public function login()
+    {
+        //validar + registrar login
+
+    }
+
+    public function logout()
+    {
+        
+    }
+
+    public function autenticar()
+    {
+        
+    }
+}
---
+++ agencia/formLogin.php
@@ -0,0 +1,23 @@
+<?php
+    require 'config/config.php';
+    include 'includes/header.php';
+?>
+    <main class="container">
+        <h1>Ingreso Sistema</h1>
+        <div class="alert bg-light border col-6 mx-auto">
+        <form action="login.php" method="post">
+            Usuario:
+            <br>
+            <input type="text" name="usuEmail"  class="form-control">
+            <br>
+            Contraseña:
+            <br>
+            <input type="password" name="usuClave" class="form-control">
+            <br>
+            <button class="btn btn-dark btn-block">Ingresar a sistema</button>
+        </form>
+        </div>
+    </main>
+<?php
+    include 'includes/footer.php';
+?>
